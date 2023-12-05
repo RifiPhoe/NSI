10c14ff994528f2e151bc27c61ca222a4c4940ff
Phœ <129071666+RifiPhoe@users.noreply.github.com>
2023-12-05 10:45:33 +0000

    uoui

--- base.py
+++ base.py
@@ -4,3 +4,9 @@ def decTob(n,b):
   assert (b>1 and b<17), "b doit être compris entre 2 et 16"
   signes = ["0","1","2","3","4","5","6","7","8","9","A","B","C","D","E","F"]
   mot = ""
+  while n!=0:
+    mot = signes[n%b] + mot
+    n = n//b
+  return mot
+
+print(decTob(15,16))
